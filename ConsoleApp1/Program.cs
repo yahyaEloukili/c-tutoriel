232b5a6132dcdb3526b6065f3dda4ccea2a75211
yahya <yahya.ensas@hotmail.com>
2020-12-16 21:53:56 +0100

    lesson 2 - concatenation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace ConsoleApp1
-{
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+        //Concatenation
+            Console.WriteLine("Please enter your name !");
+        String username = Console.ReadLine();
+        Console.WriteLine("Hello "+username);
+        //placeholder syntax - prefered
+        Console.WriteLine("Please enter your first name !");
+        String firstname = Console.ReadLine();
+        Console.WriteLine("Please enter your last name !");
+        String lastname = Console.ReadLine();
+        Console.WriteLine("Hello {0} {1} ",firstname, lastname);
+
     }
-}
+    }
+
