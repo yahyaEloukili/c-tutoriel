82cbbdb71a9dff6be4b5ae35d308e7593d876b79
yahya <yahya.ensas@hotmail.com>
2020-12-17 13:24:56 +0100

    lesson 4 - string data type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,13 +5,13 @@
     {
         static void Main(string[] args)
         {
-        //boolean type
-        bool b = true;
-        //integral type
-        int i = 123;
-        Console.WriteLine("min is {0}",int.MinValue);
-        Console.WriteLine("max is {0}", int.MaxValue);
 
+        //sequence character \n
+        string name = "\"Pragim\"";
+        Console.WriteLine(name);
+        Console.WriteLine("one\ntwo\nthree");
+        Console.WriteLine("c:\\projects");
+        Console.WriteLine(@"c:\\projects");
     }
     }
 
